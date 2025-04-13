c337cdd020407ef15daeb2863a160a57a1987053
Mohamed Sedek <MohamedSedek31@gmail.com>
2025-04-13 08:40:54 -0700

    22/2

--- src/pages/Agents/Agents.jsx
+++ src/pages/Agents/Agents.jsx
@@ -29,7 +29,7 @@ const Agents = () => {
       .catch(error => {
         console.log(token);
         console.error('Error fetching data:', error);
-      });
+      });   
       
   }, [update])
 
